41ce37cc41f6b832c68784b11e099503adab1034
David <dsinemark@gmail.com>
2022-08-31 15:00:00 +0200

    Navbar fully responsive and styled

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
+import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className='grid grid-cols-12 gap-8 px-5 lg:px-48 my-14'>
+    <div className='grid grid-cols-12 gap-8 px-5 lg:px-48 my-14 sm:px-24 md:px-32'>
       <div className='col-span-12 lg:col-span-3 bg-white rounded-2xl p-4 text-center'>
         <Sidebar/>
         </div>
       <div className='lg:col-span-9 bg-white rounded-2xl col-span-12'>
+        
         <Component {...pageProps} />
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
     fontFamily: {
       'aboreto': ['Aboreto'],
       'anton': ['Anton'],
+      'poppins': ['Poppins'],
     },
     extend: {},
   },
